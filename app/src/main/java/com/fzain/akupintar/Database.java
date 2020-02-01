3400191bdbc974e1438f5d071824f8e4019adccc
fzain <abdul.muhaimin.abas@gmail.com>
2020-02-01 18:34:55 +0800

    Aku Pintar

--- app/src/main/java/com/fzain/akupintar/Database.java
+++ app/src/main/java/com/fzain/akupintar/Database.java
@@ -6,18 +6,26 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.fzain.akupintar.Model.UniversitasModel;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Database extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "UniversitasDB";
-    private static final String T_UNIVERSITAS = "tb_notes";
+    private static final String T_UNIVERSITAS = "tb_universitas";
 
     private static final String KEY_ID = "id_universitas";
     private static final String KEY_NAMA = "nama_universitas";
     private static final String KEY_AKREDITAS = "akreditas";
     private static final String KEY_STATUS = "status";
+    private static final String KEY_LOGO = "logo";
+
+    private static final String TABLE_ALAMAT = "alamat";
+    private static final String KEY_IDA = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_IMAGE = "image";
 
     public Database(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -25,22 +33,29 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        tes(db);
+        alamat(db);
+
+    }
+
+    public void tes (SQLiteDatabase db){
         String query = "CREATE TABLE " + T_UNIVERSITAS + " (" +
                 KEY_ID + " INTEGER PRIMARY KEY, " +
                 KEY_NAMA + " TEXT, " +
                 KEY_AKREDITAS + " TEXT, " +
-                KEY_STATUS + " TEXT" + ")";
+                KEY_STATUS + " TEXT" +")";
         Log.d("Data", "onCreate: " + query);
         db.execSQL(query);
 
-        query = "insert into tb_notes values(1,'Firman','sdfsdfdsfsxczxc','xcvxcvxcvxc')";
+        query = "insert into tb_universitas values(1,'Fadel','Akreditas A','Status')";
         db.execSQL(query);
-        query = "insert into tb_notes values(2,'Vanli','sdfsdfdsfsxczxc','xcvxcvxcvxc')";
+        query = "insert into tb_universitas values(2,'Adi','Akreditas B','Status')";
         db.execSQL(query);
-        query = "insert into tb_notes values(3,'Adi','sdfsdfdsfsxczxc','xcvxcvxcvxc')";
+        query = "insert into tb_universitas values(3,'Adi','Akreditas C','Status')";
         db.execSQL(query);
-
-
+        query = "insert into tb_universitas values(4,'Fadel','Akreditas D','Status')";
+        db.execSQL(query);
     }
 
     @Override
@@ -50,12 +65,15 @@ public class Database extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + T_UNIVERSITAS);
 
         onCreate(db);
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALAMAT);
+
+        onCreate(db);
     }
 
 
-
-    public List<UniversitasModel> getNotes() {
-        List<UniversitasModel> noteList = new ArrayList<>();
+    public List<UniversitasModel> getUniv() {
+        List<UniversitasModel> univList = new ArrayList<>();
         String query = "SELECT * FROM " + T_UNIVERSITAS;
 
         SQLiteDatabase db = this.getWritableDatabase();
@@ -64,18 +82,27 @@ public class Database extends SQLiteOpenHelper {
         if (cursor.moveToFirst()) {
             do {
                 int id = cursor.getInt(0);
-                String title = cursor.getString(1);
-                String desc = cursor.getString(2);
-                String date = cursor.getString(3);
+                String nama = cursor.getString(1);
+                String akreditas = cursor.getString(2);
+                String status = cursor.getString(3);
 
-                UniversitasModel universitasModel = new UniversitasModel(id, title, desc, date);
+                UniversitasModel universitasModel = new UniversitasModel(id, nama, akreditas, status);
 
-                noteList.add(universitasModel);
+                univList.add(universitasModel);
             } while (cursor.moveToNext());
         }
 
-        return noteList;
+        return univList;
     }
 
+
+
+
+    public void alamat (SQLiteDatabase db){
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_ALAMAT + "("
+                + KEY_IDA + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_IMAGE + " BLOB" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
 
 }
--- app/src/main/java/com/fzain/akupintar/MainActivity.java
+++ app/src/main/java/com/fzain/akupintar/MainActivity.java
@@ -1,21 +1,38 @@
 package com.fzain.akupintar;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.fzain.akupintar.Adapter.SliderImageAdapter;
+import com.fzain.akupintar.Adapter.UniversitasAdapter;
+import com.fzain.akupintar.Model.UniversitasModel;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.IndicatorView.draw.controller.DrawController;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
     private RecyclerView rvList;
 
     private Database db;
     private UniversitasAdapter adapter;
-    private List<UniversitasModel> notes = new ArrayList<>();
+    private List<UniversitasModel> univer = new ArrayList<>();
+    SliderView sliderMyuniv;
+    TextView greetText;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db = new Database(this);
+        sliderMyuniv = findViewById(R.id.imageSlider);
+        greetText = findViewById(R.id.greeting_text);
 
         rvList = findViewById(R.id.rv_univ);
 
@@ -36,17 +55,48 @@ public class MainActivity extends AppCompatActivity {
         rvList.setItemAnimator(new DefaultItemAnimator());
         rvList.setAdapter(adapter);
 
+        final SliderImageAdapter sliderImageAdapter = new SliderImageAdapter(this);
+        sliderImageAdapter.setCount(4);
+        sliderMyuniv.setSliderAdapter(sliderImageAdapter);
+        sliderMyuniv.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderMyuniv.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderMyuniv.setIndicatorSelectedColor(Color.WHITE);
+        sliderMyuniv.setIndicatorUnselectedColor(Color.GRAY);
+        sliderMyuniv.startAutoCycle();
+        sliderMyuniv.setOnIndicatorClickListener(new DrawController.ClickListener() {
+            @Override
+            public void onIndicatorClicked(int position) {
+                sliderMyuniv.setCurrentPagePosition(position);
+            }
+        });
 
+        greeting();
+
+    }
+
+    private void greeting() {
+        Calendar calendar = Calendar.getInstance();
+        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (timeOfDay >= 0 && timeOfDay < 12) {
+            greetText.setText(getString(R.string.salam_pagi));
+        } else if (timeOfDay >= 12 && timeOfDay < 15) {
+            greetText.setText(getString(R.string.salam_siang));
+        } else if (timeOfDay >= 15 && timeOfDay < 18) {
+            greetText.setText(getString(R.string.salam_sore));
+        } else if (timeOfDay >= 18 && timeOfDay < 24) {
+            greetText.setText(getString(R.string.salam_malam));
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        getNotes();
+        getUnivtast();
     }
 
-    private void getNotes() {
-        notes = db.getNotes();
-        adapter.setUniver(notes);
+    private void getUnivtast() {
+        univer = db.getUniv();
+        adapter.setUniver(univer);
     }
 }
